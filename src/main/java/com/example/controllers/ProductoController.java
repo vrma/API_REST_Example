87f38c3381fcba7bcab009ef77399e1c3868716c
vrmachado <vrmachado@gmail.com>
2023-04-03 16:34:42 +0200

    FileUploadDownload finished

--- src/main/java/com/example/controllers/ProductoController.java
+++ src/main/java/com/example/controllers/ProductoController.java
@@ -7,12 +7,15 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.BindingResult;
@@ -30,13 +33,17 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.example.entities.Producto;
+import com.example.model.FileUploadResponse;
 import com.example.services.ProductoService;
+import com.example.utilities.FileDownloadUtil;
 import com.example.utilities.FileUploadUtil;
 
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/productos")
+@RequiredArgsConstructor
 public class ProductoController {
 
     @Autowired
@@ -45,6 +52,8 @@ public class ProductoController {
     @Autowired
     private FileUploadUtil fileUploadUtil;
 
+    // Se inyectará por constructor
+    private final FileDownloadUtil fileDownloadUtil;
     /**
      * 
      * El metodo siguiente va a responder a una peticion (request) del tipo:
@@ -172,6 +181,19 @@ public class ProductoController {
         if(!file.isEmpty()) {
             String fileCode = fileUploadUtil.saveFile(file.getOriginalFilename(), file);
             producto.setImagenProducto(fileCode+ "-" + file.getOriginalFilename());
+
+            // Devolver informacion respecto al file recibido
+
+            FileUploadResponse fileUploadResponse = FileUploadResponse
+                       .builder()
+                       .fileName(fileCode + "-" + file.getOriginalFilename())
+                       .downloadURI("/productos/downloadFile/" 
+                                 + fileCode + "-" + file.getOriginalFilename())
+                       .size(file.getSize())
+                       .build();
+            
+            responseAsMap.put("info de la imagen: ", fileUploadResponse);           
+
         }
 
         Producto productoDB =  productoService.save(producto);
@@ -283,6 +305,33 @@ public class ProductoController {
     }
 
 
+    /**
+     *  Implementa filedownnload end point API 
+     **/    
+    @GetMapping("/downloadFile/{fileCode}")
+    public ResponseEntity<?> downloadFile(@PathVariable(name = "fileCode") String fileCode) {
+
+        Resource resource = null;
+
+        try {
+            resource = fileDownloadUtil.getFileAsResource(fileCode);
+        } catch (IOException e) {
+            return ResponseEntity.internalServerError().build();
+        }
+
+        if (resource == null) {
+            return new ResponseEntity<>("File not found ", HttpStatus.NOT_FOUND);
+        }
+
+        String contentType = "application/octet-stream";
+        String headerValue = "attachment; filename=\"" + resource.getFilename() + "\"";
+
+        return ResponseEntity.ok()
+        .contentType(MediaType.parseMediaType(contentType))
+        .header(HttpHeaders.CONTENT_DISPOSITION, headerValue)
+        .body(resource);
+
+    }    
 
     /**
      * El método siguiente es de ejemplo para entender el formato JSON
---
+++ src/main/java/com/example/model/FileUploadResponse.java
@@ -0,0 +1,17 @@
+package com.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class FileUploadResponse {
+    
+    private String fileName;
+    private String downloadURI;
+    private long size;
+}
